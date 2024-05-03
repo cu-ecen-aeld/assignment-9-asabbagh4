9418bde010158d82f6203f53a45c434aa32bd1b0
Abdul Alsabbagh <atsabbagh@gmail.com>
2024-05-03 19:25:28 -0400

    Last attempt for assignment 9

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '65595de636d20af68beb55ce504238edda96c0d7'
+AESD_ASSIGNMENTS_VERSION = '4326572b37494ba1e34ed88f90ddbd8a1cba2bf0'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesd-char-driver/aesd-char-driver.mk
+++ base_external/package/aesd-char-driver/aesd-char-driver.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESD_CHAR_DRIVER_VERSION = '65595de636d20af68beb55ce504238edda96c0d7'
+AESD_CHAR_DRIVER_VERSION = '4326572b37494ba1e34ed88f90ddbd8a1cba2bf0'
 
 AESD_CHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-asabbagh4.git'
 AESD_CHAR_DRIVER_SITE_METHOD = git
@@ -13,7 +13,7 @@ AESD_CHAR_DRIVER_GIT_SUBMODULES = YES
 AESD_CHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
 
 define AESD_CHAR_DRIVER_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load   $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_unload $(TARGET_DIR)/usr/bin
 endef
 
